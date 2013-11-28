3b07b6dfbf5e96d7d3c515458d5cf618b6f75774
mrcraig <1002386c@student.gla.ac.uk>
2013-11-28 22:47:48 +0000

    Started 3, removed b/f from 1

--- Task3/src/T3Reducer.java
+++ Task3/src/T3Reducer.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
 
 
-public class T1Reducer extends TableReducer<ImmutableBytesWritable, LongWritable, ImmutableBytesWritable> {
+public class T3Reducer extends TableReducer<ImmutableBytesWritable, LongWritable, ImmutableBytesWritable> {
 	
 	public void reduce(ImmutableBytesWritable key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
 		//Break current key into artID
@@ -19,7 +19,8 @@ public class T1Reducer extends TableReducer<ImmutableBytesWritable, LongWritable
 		Put put = new Put(artID);
 		for(LongWritable v:values){
 			//Emit KV pairs (should be already sorted)
-			put.add(Bytes.toBytes("q1"), Bytes.toBytes("revid"), Bytes.toBytes(v.get()));
+			System.out.println(Bytes.toLong(artID) + "--" + v.get());
+			put.add(Bytes.toBytes("q3"), Bytes.toBytes("revid"), Bytes.toBytes(v.get()));
 		}
 		context.write(null, put);
 	}
